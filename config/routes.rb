86a107e29958e9a57b04e89a6cece54481272dba
john-nci <x17147123@student.ncirl.ie>
2018-07-01 22:46:21 +0000

    Added Questions model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :polls
+  resources :polls do 
+    resources :questions
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'polls#index' #Rooting straight to polls page
 end
